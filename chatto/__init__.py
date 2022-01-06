89c94f84d3d7b171b775337d91cee61566ad04ef
Ethan Henderson <ethan.henderson.1998@gmail.com>
2022-01-06 18:40:14 +0000

    Bump version number

--- chatto/__init__.py
+++ chatto/__init__.py
@@ -29,7 +29,7 @@
 __all__ = ("YOUTUBE_API_BASE_URL", "YOUTUBE_API_SCOPES", "YouTubeBot", "ux")
 
 __productname__ = "chatto"
-__version__ = "0.2.0.post1"
+__version__ = "0.2.1"
 __description__ = "A unified API wrapper for YouTube and Twitch chat bots."
 __url__ = "https://github.com/parafoxia/chatto"
 __docs__ = "https://chatto.readthedocs.io/en/latest"
